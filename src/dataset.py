eba7d71e0afa76aa3ecb3e158396150013850bd4
Gus <gus@ef.je>
2025-09-27 23:11:34 +1000

    feat: dataset overhaul

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,9 @@
 import os
 import sys
+import glob
+import h5py
+import random
+import numpy as np
 
 import torch
 import torchvision as tv
@@ -14,7 +18,12 @@ from utils.img import *
 
 
 def process_imgs(img_paths: list[str]) -> TensorType["batch", 3, 112, 128]:
-    imgs = torch.stack([scale_img(read_img(path)) for path in img_paths])
+    imgs = torch.stack(
+        [
+            scale_img(read_img(path))
+            for path in tqdm(img_paths, desc="Processing images")
+        ]
+    )
 
     imgs = vrgb_to_lab(imgs)
 
@@ -30,21 +39,57 @@ def process_imgs(img_paths: list[str]) -> TensorType["batch", 3, 112, 128]:
     return imgs.to(torch.uint8)
 
 
+def save_chunk(chunks: list[list[str]], output_path: str, chunks_size: int):
+    length = sum(len(chunk) for chunk in chunks)
+
+    print(f"Processing {len(chunks)} chunks into {output_path}")
+
+    with h5py.File(output_path, "w") as f:
+        ds = f.create_dataset(
+            "images",
+            shape=(length, 4, 112, 128),
+            dtype="uint8",
+            compression="gzip",
+            chunks=(chunks_size, 4, 112, 128),
+        )
+
+        start = 0
+        for i, chunk in tqdm(enumerate(chunks), total=len(chunks)):
+            imgs = process_imgs(chunk)
+            end = start + imgs.shape[0]
+            ds[start:end] = imgs
+            start = end
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python dataset.py <input_dir> <output_dir> <chunk_size>")
+        print("Usage: python dataset.py <input_dir> <output_path> <chunk_size>")
         sys.exit(1)
 
-    imgs = os.listdir(sys.argv[1])
-    output_dir = sys.argv[2]
+    imgs = glob.glob(os.path.join(sys.argv[1], "**", "*.jpg"), recursive=True)
+    output_path = sys.argv[2]
     chunks_size = int(sys.argv[3])
 
-    chunks = [imgs[i:i + chunks_size]
-              for i in range(0, len(imgs), chunks_size)]
+    res = process_imgs(imgs)
+    res = res.detach().cpu().numpy().astype(np.uint8)
 
-    print(f"Processing {len(chunks)} chunks into {output_dir}")
+    np.savez_compressed(output_path, imgs=res)
 
-    for i, chunk in tqdm(enumerate(chunks), total=len(chunks)):
-        imgs = process_imgs([os.path.join(sys.argv[1], p) for p in chunk])
+    # chunks = [imgs[i : i + chunks_size] for i in range(0, len(imgs), chunks_size)]
 
-        torch.save(imgs, os.path.join(output_dir, f"{i}.pt"))
+    # random.shuffle(chunks)
+
+    # n = len(chunks)
+    # n_train = int(n * 0.7)
+    # n_test = int(n * 0.2)
+    # n_val = n - n_train - n_test
+
+    # train_chunks = chunks[:n_train]
+    # test_chunks = chunks[n_train:n_train + n_test]
+    # val_chunks = chunks[n_train + n_test:]
+
+    # print(f"Train chunks: {len(train_chunks)}, Test chunks: {len(test_chunks)}, Val chunks: {len(val_chunks)}")
+
+    # save_chunk(train_chunks, f"{output_path}_train.h5", chunks_size)
+    # save_chunk(test_chunks, f"{output_path}_test.h5", chunks_size)
+    # save_chunk(val_chunks, f"{output_path}_val.h5", chunks_size)
