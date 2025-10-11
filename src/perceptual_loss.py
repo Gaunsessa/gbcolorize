8eda6a63ecad70d19b87356a3a28054affbaeb84
Gus <gus@ef.je>
2025-10-11 23:50:43 +1100

    feat: update percep loss

--- src/perceptual_loss.py
+++ src/perceptual_loss.py
@@ -10,11 +10,18 @@ class PerceptualLoss(nn.Module):
     def __init__(self):
         super(PerceptualLoss, self).__init__()
 
-        self.vgg16 = models.vgg16(weights=models.VGG16_Weights.DEFAULT).features
+        vgg16 = models.vgg16(weights=models.VGG16_Weights.DEFAULT).features
+
+        self.selected_layers = [3, 8, 15]
+        self.vgg16 = nn.Sequential(*list(vgg16)[:16])
         self.vgg16.eval()
 
         for param in self.vgg16.parameters():
             param.requires_grad = False
+
+        # ImageNet normalization
+        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1,3,1,1))
+        self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1,3,1,1))
 
     def forward(self, grey, x_ab, y_ab):
         grey = grey.clone()
@@ -30,11 +37,16 @@ class PerceptualLoss(nn.Module):
         x = torch.vmap(lab_to_rgb)(x.view(x.shape[0], 3, -1)).view(x.shape[0], 3, 112, 128)
         y = torch.vmap(lab_to_rgb)(y.view(y.shape[0], 3, -1)).view(y.shape[0], 3, 112, 128)
 
-        # ImageNet mean and std
-        mean = torch.tensor([0.485, 0.456, 0.406], device=x.device).view(1, 3, 1, 1)
-        std  = torch.tensor([0.229, 0.224, 0.225], device=x.device).view(1, 3, 1, 1)
+        x = (x - self.mean) / self.std
+        y = (y - self.mean) / self.std
 
-        x = (x - mean) / std
-        y = (y - mean) / std
+        loss = 0.0
 
-        return torch.nn.functional.mse_loss(self.vgg16(x), self.vgg16(y))
+        for i, layer in enumerate(self.vgg16):
+            x = layer(x)
+            y = layer(y)
+            
+            if i in self.selected_layers:
+                loss += nn.functional.mse_loss(x, y)
+
+        return loss
