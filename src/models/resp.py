75589e5bd0100a0d1fdaf247ad8de58878f5881e
Gus <gus@ef.je>
2025-11-05 13:26:11 +1100

    feat: expanded resp test

--- src/models/resp.py
+++ src/models/resp.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as tf
 import torchvision.models as models
 
 
@@ -9,21 +10,7 @@ class RespModel(nn.Module):
 
         resnet34 = models.resnet34(weights=models.ResNet34_Weights.DEFAULT)
 
-        # self.frozen = False
-
-        self.expand = nn.Sequential(
-            nn.Conv2d(1, 32, 5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(),
-            nn.Conv2d(32, 64, 5, stride=1, padding=2),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(),
-            nn.Conv2d(64, 32, 5, stride=1, padding=2),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(),
-            nn.Conv2d(32, 3, 5, stride=1, padding=2),
-            nn.LeakyReLU(),
-        )
+        self.expand = nn.Conv2d(1, 3, 1, stride=1)
 
         self.encoder = nn.ModuleList(
             [
@@ -54,16 +41,22 @@ class RespModel(nn.Module):
                     nn.ConvTranspose2d(256, 128, 4, stride=2, padding=1, bias=False),
                     nn.BatchNorm2d(128),
                     nn.LeakyReLU(),
+                    nn.ConvTranspose2d(128, 128, 4, stride=1, padding=1, bias=False),
+                    nn.BatchNorm2d(128),
                 ),
                 nn.Sequential(
                     nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1, bias=False),
                     nn.BatchNorm2d(64),
                     nn.LeakyReLU(),
+                    nn.ConvTranspose2d(64, 64, 4, stride=1, padding=1, bias=False),
+                    nn.BatchNorm2d(64),
                 ),
                 nn.Sequential(
                     nn.ConvTranspose2d(64, 64, 4, stride=2, padding=1, bias=False),
                     nn.BatchNorm2d(64),
                     nn.LeakyReLU(),
+                    nn.ConvTranspose2d(64, 64, 4, stride=1, padding=1, bias=False),
+                    nn.BatchNorm2d(64),
                 ),
                 nn.Sequential(
                     nn.ConvTranspose2d(64, 256, 4, stride=2, padding=1),
@@ -86,38 +79,23 @@ class RespModel(nn.Module):
 
             if i != len(self.decoder) - 1:
                 x = x + encodes[-i - 1]
+                x = tf.leaky_relu(x)
 
         return x
 
-    # def train(self, mode: bool = True):
-    #     self.training = mode
-
-    #     self.encoder.train(mode)
-
-    #     self.decoder.train(mode and not self.frozen)
-    #     self.bottleneck.train(mode and not self.frozen)
-
-    # def eval(self):
-    #     self.training = False
-
-    #     self.encoder.eval()
-    #     self.bottleneck.eval()
-    #     self.decoder.eval()
-
     def freeze_encoder(self, freeze: bool = True):
-        # self.frozen = freeze
-
-        # self.encoder.train(not freeze)
-        # self.bottleneck.train(not freeze)
-
         for param in self.encoder_params:
             param.requires_grad = not freeze
 
     def init_weights(self):
+        self.expand.weight.data.fill_(1)
+        if self.expand.bias is not None:
+            self.expand.bias.data.fill_(0)
+        
         for layer in self.decoder.modules():
             if isinstance(layer, nn.ConvTranspose2d):
                 nn.init.kaiming_normal_(
-                    layer.weight, mode="fan_out", nonlinearity="relu"
+                    layer.weight, mode="fan_out", nonlinearity="leaky_relu"
                 )
 
                 if layer.bias is not None:
