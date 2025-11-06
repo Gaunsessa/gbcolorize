cd4ebff1bbbb5efd48b4e477efa9ec9d89d33389
Gus <gus@ef.je>
2025-11-06 16:17:19 +1100

    feat: registered colorloss buffers

--- src/color_loss.py
+++ src/color_loss.py
@@ -4,30 +4,36 @@ import torch.nn.functional as tf
 
 
 class ColorLoss(nn.Module):
+    knn_idx: torch.Tensor
+    knn_weights: torch.Tensor
+    weights: torch.Tensor
+
     def __init__(
-        self, knn_idx: torch.Tensor, knn_weights: torch.Tensor, weights: torch.Tensor
+        self,
+        knn_idx: torch.Tensor,
+        knn_weights: torch.Tensor,
+        weights: torch.Tensor,
     ):
         super().__init__()
-        
-        self.knn_idx = knn_idx
-        self.knn_weights = knn_weights
-        self.weights = weights
-        
+
+        self.register_buffer("knn_idx", knn_idx)
+        self.register_buffer("knn_weights", knn_weights)
+        self.register_buffer("weights", weights)
+
     def forward(self, pred, target):
         prob = tf.log_softmax(pred, dim=1)
-    
+        
         target_idx = target.squeeze(1)
-    
+
         idx = self.knn_idx[target_idx]
         wts = self.knn_weights[target_idx]
-    
         weights = self.weights[target_idx]
-    
+
         idx = idx.permute(0, 3, 1, 2)
         wts = wts.permute(0, 3, 1, 2)
-    
+
         p = torch.gather(prob, 1, idx)
-    
+
         loss = -((p * wts).sum(dim=1) * weights).mean()
-    
+
         return loss
