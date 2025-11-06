0ada96800f8babd9d349118ba377faae24946608
Gus <gus@ef.je>
2025-11-06 17:39:12 +1100

    feat: shared dataset memory

--- src/train.py
+++ src/train.py
@@ -7,9 +7,11 @@ import numpy as np
 
 from torch.utils.data import DataLoader
 
-from lightning import Trainer
+from lightning import LightningDataModule, Trainer
 from lightning.pytorch.loggers import TensorBoardLogger
 from lightning.pytorch.strategies import DDPStrategy
+
+from tqdm import tqdm
 
 from dataloader import GBColorizeDataset
 from color_loss import ColorLoss
@@ -31,19 +33,44 @@ def load_dataset(path: str):
         ]
     )
 
-    for path in paths:
+    for path in tqdm(paths, desc="Loading dataset"):
         data = np.load(path)
         luma = torch.cat((luma, torch.tensor(data["luma"])), dim=0)
         color = torch.cat((color, torch.tensor(data["color"], dtype=torch.long)), dim=0)
 
+    luma.share_memory_()
+    color.share_memory_()
+
     return luma, color
+
+
+class GBColorizeDataModule(LightningDataModule):
+    def __init__(self, dataset_path: str, batch_size=64, num_workers=4):
+        super().__init__()
+        self.luma, self.color = load_dataset(dataset_path)
+
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def setup(self, stage=None):
+        # Wrap shared memory tensors into your Dataset
+        self.dataset = GBColorizeDataset(self.luma, self.color)
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            persistent_workers=True,
+            shuffle=True,
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, required=True)
     args = parser.parse_args()
-    
+
     bins = get_color_bins()
     bin_knn_idx, bin_knn_weights = precompute_color_bins_weights(bins)
     bin_weights = torch.load(os.path.join(args.dataset, "bin_weights.pt"))
@@ -53,17 +80,10 @@ if __name__ == "__main__":
     model = EfficientModel(0, len(bins), loss_fn)
     model.init_weights()
     model.freeze_encoder()
-    
-    luma, color = load_dataset(args.dataset)
-    luma.share_memory_()
-    color.share_memory_()
-    
-    dataset = GBColorizeDataset(luma, color)
-    dataloader = DataLoader(
-        dataset, batch_size=64, num_workers=4, persistent_workers=True
-    )
+
+    datamodule = GBColorizeDataModule(args.dataset)
 
     logger = TensorBoardLogger(save_dir="runs")
     trainer = Trainer(logger=logger, strategy=DDPStrategy(find_unused_parameters=True))
-    
-    trainer.fit(model=model, train_dataloaders=dataloader)
+
+    trainer.fit(model=model, datamodule=datamodule)
