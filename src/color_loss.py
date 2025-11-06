b3fa86c32b9ee384b73fc1487d634842c4a079c5
Gus <gus@ef.je>
2025-11-06 12:55:18 +1100

    feat: turned colorloss into a module

--- src/color_loss.py
+++ src/color_loss.py
@@ -1,28 +1,33 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as tf
 
 
-def cross_entropy_color_loss(
-    pred: torch.Tensor,
-    target: torch.Tensor,
-    knn_idx: torch.Tensor,
-    knn_weights: torch.Tensor,
-    weights: torch.Tensor,
-) -> torch.Tensor:
-    prob = tf.log_softmax(pred, dim=1)
-
-    target_idx = target.squeeze(1)
-
-    idx = knn_idx[target_idx]
-    wts = knn_weights[target_idx]
-
-    weights = weights[target_idx]
-
-    idx = idx.permute(0, 3, 1, 2)
-    wts = wts.permute(0, 3, 1, 2)
-
-    p = torch.gather(prob, 1, idx)
-
-    loss = -((p * wts).sum(dim=1) * weights).mean()
-
-    return loss
+class ColorLoss(nn.Module):
+    def __init__(
+        self, knn_idx: torch.Tensor, knn_weights: torch.Tensor, weights: torch.Tensor
+    ):
+        super().__init__()
+        
+        self.knn_idx = knn_idx
+        self.knn_weights = knn_weights
+        self.weights = weights
+        
+    def forward(self, pred, target):
+        prob = tf.log_softmax(pred, dim=1)
+    
+        target_idx = target.squeeze(1)
+    
+        idx = self.knn_idx[target_idx]
+        wts = self.knn_weights[target_idx]
+    
+        weights = self.weights[target_idx]
+    
+        idx = idx.permute(0, 3, 1, 2)
+        wts = wts.permute(0, 3, 1, 2)
+    
+        p = torch.gather(prob, 1, idx)
+    
+        loss = -((p * wts).sum(dim=1) * weights).mean()
+    
+        return loss
